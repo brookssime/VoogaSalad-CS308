cbdf45805147a954843292f59a7c97922024ca64
sidgopinath <sdg23@duke.edu>
2015-03-27 03:17:59 -0400

    Made gameEngine interfaces and several skeleton classes for future use.

---
+++ code_interfaces/game_engine/Sprite.java
@@ -0,0 +1,17 @@
+package game_engine;
+
+/**
+ * This is the Sprite abstract interface
+ * Sprites such as Tower, Base, Enemy, and Projectile will extend this
+ * There are no methods because the sprite object is going to be very low in the hierarchy
+ * However, each sprite will hold instance variables, such as its health.
+ * The SpriteManager class (detailed in Design Details) will handle moving the sprites
+ * That SpriteManager class is also going to handle collisions, deaths, etc.
+ * @author Sid
+ *
+ */
+
+
+public abstract interface Sprite {
+
+}
