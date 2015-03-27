ad223cd267cad8636a3b2234d50952c11e894457
Sunjeev <svd12@duke.edu>
2015-03-27 03:45:00 -0400

    Very Rough Example Code Added

---
+++ src/voogasalad_TuffWizard/UseCase3.java
@@ -0,0 +1,26 @@
+package voogasalad_TuffWizard;
+
+/*
+ *Player Kills the last enemy in the last wave and wins the game 
+ */
+public class UseCase3 {
+
+// 1) Tower Kills the Enemy
+//		spritemanager.checkTarget() 
+//			checks if enemy is in range
+//		tower.fire()
+//			If enemy is in range it fires at the enemy
+//		check for collisions
+//			Check for projectile collision with enemy (and all other collisions)
+//		sprite.update
+//u			pdate sprite according to its state
+//
+// 2) Scene is updated
+//	LoadScene.isComplete() 
+//		This method is constantly checking for complete scene; when it sees the last enemy is dead, 
+//		it calls this method.
+// 3) Game.setScene(myHead.myNext)
+//		Scene is set to the next in the linked list
+//		Game sets timeline for the GUI and the new scene.
+
+}
