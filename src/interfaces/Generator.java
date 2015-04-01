30c6cf898bfd471c5c99744cb900a5782e4e16be
Brooks Sime <brookssime@gmail.com>
2015-04-01 19:20:47 -0400

    additional comments

--- src/interfaces/Generator.java
+++ src/interfaces/Generator.java
@@ -3,6 +3,9 @@ package interfaces;
 /**
  * The Generator interface handles objects that generate other objects.
  * Ex. A tower can GENERATE a projectile
+ * Ex. a wave can GENERATE a enemy
+ * Ex. a store can GENERATE a tower
+ * Ex. a enemy can GENERATE other enemies
  * @author brookssime
  * 
  *
