53e2f0afe0cc3d26956c18d8d0b0f145626cb84d
Brooks Sime <brookssime@gmail.com>
2015-04-01 19:06:22 -0400

    added comments to generator and generatable, added position to displayable

--- src/interfaces/Displayable.java
+++ src/interfaces/Displayable.java
@@ -4,5 +4,6 @@ public interface Displayable {
 	
 	void setImage();
 	void setVisible(boolean visible);
+	void Position();
 
 }
--- src/interfaces/Generator.java
+++ src/interfaces/Generator.java
@@ -1,5 +1,12 @@
 package interfaces;
 
+/**
+ * The Generator interface handles objects that generate other objects.
+ * Ex. A tower can GENERATE a projectile
+ * @author brookssime
+ * 
+ *
+ */
 public interface Generator {
 
 	void setPort(double x, double y);
