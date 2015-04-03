f22197b1989cce1ecc1da83492d12c9c86a07f39
Brooks Sime <brookssime@gmail.com>
2015-04-02 21:29:05 -0400

    initial commit

--- src/interfaces/Movable.java
+++ src/interfaces/Movable.java
@@ -1,5 +1,12 @@
 package interfaces;
 
+/**
+ * handle commands like spin, walk, fly, teleport, etc.
+ * @author brookssime
+ *
+ */
+
+
 public interface Movable {
 	
 	void setMover(Mover mover);
--- src/interfaces/Mover.java
+++ src/interfaces/Mover.java
@@ -1,5 +1,15 @@
 package interfaces;
-
+/**
+ * Handles objects that can move & path-finding interface
+ * 
+ * Towers, Enemies
+ * Ex. Path enemies take
+ * Ex. Path projectiles take
+ * Ex. 
+ * @author brookssime
+ *
+ */
 public interface Mover {
+	
 
 }
