dbe07f9001ea94c0bfc255fa95f08738ec6b469e
Robert Vann <robert.vann@duke.edu>
2015-04-09 13:28:06 -0400

    Implemented Wave class and updated LevelScene

--- src/GameEngine/LevelScene.java
+++ src/GameEngine/LevelScene.java
@@ -5,29 +5,31 @@ import interfaces.Movable;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+public class LevelScene extends GameScene {
 
-public class LevelScene extends GameScene{
-
-	private List<Movable> myMovables; 
+	private List<Movable> myMovables;
 	private List<Collidable> myCollidables;
+	private LinkedList<Wave> myWaves;
 	private Base myBase;
 	private Set<Collidable> mySpritesToRemove;
-	private String myLevelTitle; 
-	
+	private String myLevelTitle;
+
 	/**
 	 * TODO: How are all of these lists being populated?
 	 */
-	public LevelScene(){
+	public LevelScene() {
+		super();
 		myMovables = new ArrayList<Movable>();
 		myCollidables = new ArrayList<Collidable>();
 		mySpritesToRemove = new HashSet<Collidable>();
 	}
-	
-	public void update(){	
+
+	public void update() {
 		moveSprites();
 		checkCollisions();
 		clearSprites();
@@ -37,7 +39,7 @@ public class LevelScene extends GameScene{
 	}
 
 	private void moveSprites() {
-		for(Movable sprite: myMovables){
+		for (Movable sprite : myMovables) {
 			sprite.move();
 		}
 	}
@@ -46,7 +48,7 @@ public class LevelScene extends GameScene{
 	 * Clean this up if time?
 	 */
 	private void checkCollisions() {
-		for(Collidable sprite: myCollidables){
+		for (Collidable sprite : myCollidables) {
 			for (Collidable collider : myCollidables) {
 				if (!(sprite.equals(collider))
 						&& mySpritesToRemove.contains(collider)
@@ -60,42 +62,47 @@ public class LevelScene extends GameScene{
 	}
 
 	private void clearSprites() {
-		mySpritesToRemove.addAll(myCollidables.stream()
-				.filter(s -> s.isDead()).collect(Collectors.toSet())); //filter to find dead objects
-		for(Collidable sprite: mySpritesToRemove){
+		mySpritesToRemove.addAll(myCollidables.stream().filter(s -> s.isDead())
+				.collect(Collectors.toSet())); // filter to find dead objects
+		for (Collidable sprite : mySpritesToRemove) {
 			myCollidables.remove(sprite);
 		}
 		mySpritesToRemove.clear();
 	}
 
 	/**
-	 * TODO: Figure out timing and how to space out enemies within wave
-	 * What data structure do we use for waves?
+	 * TODO: Figure out timing and how to space out enemies within wave What
+	 * data structure do we use for waves?
 	 */
 	private void spawnEnemies() {
-		// TODO Auto-generated method stub
+		while (!myWaves.peek().isComplete()) {
+			List<Enemy> spawnedEnemies = myWaves.peek().update(myStartTime);
+			while (spawnedEnemies != null) {
+				myMovables.addAll(spawnedEnemies);
+				myCollidables.addAll(spawnedEnemies);
+			}
+		}
+		myWaves.pop();
+
 	}
 
 	/**
 	 * To be implemented later
 	 */
 	private void deployTimeBasedEffects() {
-		
+
 	}
 
 	@Override
 	public void checkComplete() {
-		if(myBase.isDead()){
+		if (myBase.isDead()) {
 			myGameLost = true;
 			myHasCompleted = true;
-		}
-		else if (myGameWon == true){
+		} else if (myGameWon == true) {
 			myHasCompleted = true;
-		}
-		else{
+		} else {
 			myHasCompleted = false;
 		}
-	}	
-	
-	
+	}
+
 }
