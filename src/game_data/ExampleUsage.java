99751617e53ba2f7f3f7d6516be0070b10d603ea
Negatu <nnegatu@gmail.com>
2015-04-18 05:05:53 -0400

    added commenting to game_data

--- src/game_data/ExampleUsage.java
+++ src/game_data/ExampleUsage.java
@@ -5,8 +5,9 @@ import java.io.IOException;
 import engine.Tower;
 
 /**
- * an example use of the GameData class
- * takes in two towers with names and saves tower 1 and overwrites tower 2 with loaded version of tower 1. 
+ * an example use of the GameData class takes in two towers with names and saves
+ * tower 1 and overwrites tower 2 with loaded version of tower 1.
+ * 
  * @author Negatu
  *
  */
@@ -14,36 +15,32 @@ import engine.Tower;
 public class ExampleUsage {
 
 	public static void main(String[] args) {
-		
+
 		Tower t1 = new Tower();
 		Tower t2 = new Tower();
-		
+
 		t1.setName("tower 1");
 		t2.setName("tower 2");
-		
-		//saving t1 to a file named by the user's choice.
+
+		// saving t1 to a file named by the user's choice.
 		try {
 			GameData.SaveGameData(t1);
 		} catch (IOException e) {
-			System.out.println("Failed to save file: "+e);
+			System.out.println("Failed to save file: " + e);
 		}
-		
-		//loading the file into t2
+
+		// loading the file into t2
 		try {
 			t2 = (Tower) GameData.LoadGameData();
 		} catch (ClassNotFoundException e) {
-			System.out.println("Class not found: "+e);
+			System.out.println("Class not found: " + e);
 		} catch (IOException e) {
-			System.out.println("IOException: "+e);
+			System.out.println("IOException: " + e);
 		}
-		
-		
-		//test if t2 is now t1
+
+		// test if t2 is now t1
 		System.out.println(t2.getName());
-		
-		
-		
-		
+
 	}
 
 }
