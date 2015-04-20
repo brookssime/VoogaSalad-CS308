a744bd5c86c699f467f3ada37794d5b717da04cf
Sunjeev <svd12@duke.edu>
2015-04-19 20:23:36 -0400

    began framework for TitleScreenEditor

--- src/gae/view/editorpane/TitleScreenEditor.java
+++ src/gae/view/editorpane/TitleScreenEditor.java
@@ -1,15 +1,23 @@
 package gae.view.editorpane;
 
 import gae.BundleGrabber;
+import gae.editorComponents.EditorComponent;
+import gae.model.Receiver;
 import gae.view.GAEPane;
 import gae.view.menupane.MenuAdder;
 
 import java.io.File;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import reflection.Reflection;
+import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.Tab;
@@ -31,74 +39,37 @@ import javafx.stage.Stage;
  * menu.
  *
  */
-public class TitleScreenEditor extends GAEPane{
+public class TitleScreenEditor extends EditorPane{
 	
+	private static final int VBOX_PADDING = 25;
+	private static final int VBOX_SPACING = 10;
 	private final ResourceBundle myConfigs;
 
-	public TitleScreenEditor(MenuAdder adder) {
-		super(TitleScreenEditor.class.getSimpleName(), adder);
+	public TitleScreenEditor(MenuAdder adder, Receiver receiver) {
+		super(adder, receiver);
 		myConfigs = BundleGrabber.grabBundle("configs", TitleScreenEditor.class.getSimpleName());
 		myRoot.getChildren().add(setRootProperties());
-		
 	}
 	
-
 	private VBox setRootProperties(){
-		VBox group = new VBox(10);
+		VBox titleScreenComponents = new VBox(VBOX_SPACING);
+		titleScreenComponents.setPadding(new Insets(VBOX_PADDING));	
+		String className = "engine.TitleScene";
+		Object myObject = Reflection.createInstance(className);
+		ArrayList<Method> objMethods = new ArrayList<Method>(
+				Reflection.getEditorMethods(myObject.getClass().toString()));
 		
-		//For setting title name
-		HBox titleSet = new HBox(5);
-		titleSet.setPadding(new Insets(25));
+		for(Method method : objMethods){
+			titleScreenComponents.getChildren().add(getComponent(method, myObject));
+		}
 		
-		group.getChildren().add(addField(myConfigs.getString("set_title"), 
-				myConfigs.getString("set_x"), myConfigs.getString("set_y")));
-		group.getChildren().add(addField(myConfigs.getString("set_play"), 
-				myConfigs.getString("set_x"), myConfigs.getString("set_y")));
-		
-		HBox fileButtonBox = new HBox(10);
-		group.getChildren().add(fileButtonBox);
-		Button openFileButton = new Button(myConfigs.getString("open_file"));
-		fileButtonBox.getChildren().add(openFileButton);
-		group.getChildren().add(openFileButton);
-		
-		openFileButton.setOnAction(e -> {
-			FileChooser fileChooser = new FileChooser();
-			fileChooser.setTitle(myConfigs.getString("open_resource"));
-			File imageFile = fileChooser.showOpenDialog(new Stage());
-			Text imagePath = new Text(imageFile.getAbsolutePath());
-			fileButtonBox.getChildren().add(imagePath);
-		});
-		
-		Button applyButton = new Button(myConfigs.getString("update"));
-		group.getChildren().add(applyButton);
-		applyButton.setOnAction(e -> {
-			//TODO: backend call
-		});
-		return group;
-	}
-	
-	private HBox addField(String item1, String item2, String item3){
-		HBox hBox = new HBox(5);
-		hBox.setPadding(new Insets(25));
-		hBox.getChildren().add(addTextField(item1));
-		hBox.getChildren().add(addTextField(item2));
-		hBox.getChildren().add(addTextField(item3));
-		return hBox;
-	}
-	
-	private HBox addTextField(String name){
-		HBox hBox = new HBox(5);
-		Text text = new Text(name);
-		TextField textField = new TextField();
-		hBox.getChildren().add(text);
-		hBox.getChildren().add(textField);
-		return hBox;
-	}
-
-	@Override
-	public List<Menu> getMenus() {
-		// TODO Auto-generated method stub
 		return null;
 	}
+
+	private EditorComponent getComponent(Method method, Object myObject) {
+		
+		return null;
+	}
+
 
 }
