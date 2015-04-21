2f9da9c47e9f6b12ee9e34183c1819fcdf01af8a
sidgopinath <sdg23@duke.edu>
2015-04-21 13:33:30 -0400

    Updated Game, Level, and TitleScene classes. Cleaned up methods and refactored some.

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,13 +1,10 @@
 package engine;
 
-import java.util.List;
-
+import javafx.animation.KeyFrame;
+import javafx.util.Duration;
 import engine.gameScreens.GameScene;
 import engine.gameScreens.LevelScene;
 import engine.gameScreens.Store;
-import interfaces.Authorable;
-import javafx.animation.KeyFrame;
-import javafx.util.Duration;
 
 /**
  * The Class Game.
@@ -19,8 +16,7 @@ public class Game {
 	
 	/** The my name. */
 	private String myName;
-	
-	/** The frame rate. */
+
 	private final int FRAME_RATE = 10;
 	
 	/** The my head. */
@@ -96,6 +92,4 @@ public class Game {
 	public boolean sceneComplete(){
 		return myHead.isComplete();
 	}
-
-	
 }
