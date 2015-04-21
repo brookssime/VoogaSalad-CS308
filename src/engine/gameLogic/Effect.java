ef9ee214351f8c8240509d51772f7ca2980f36b1
sidgopinath <sdg23@duke.edu>
2015-04-21 13:47:32 -0400

    Updated Effect object.

--- src/engine/gameLogic/Effect.java
+++ src/engine/gameLogic/Effect.java
@@ -1,36 +1,16 @@
 package engine.gameLogic;
 
-
-/**
- * The Class Effect.
- * 
- * @author Brooks, Patrick, Robert, and Sid. 
- */
 public class Effect extends GameObject{
-	
-	/** The my name. */
-	private String myName;
-	
-	/** The my speed damage. */
+
 	private Integer mySpeedDamage;
 	private Double mySpeedFrequency;
 	private Integer mySpeedReps;
 	private Double mySpeedDuration;
-	
-	/** The my health damage. */
+
 	private Integer myHealthDamage;
 	private Double myHealthFrequency;
 	private Integer myHealthReps;
-	
-	/** The is final. */
-	private boolean isFinal;
-	
-	/** The my duration. */
-	private long myDuration;
 
-	/**
-	 * Instantiates a new effect.
-	 */
 	public Effect(){
 		
 	}
@@ -85,5 +65,9 @@ public class Effect extends GameObject{
 	
 	public int getHealthReps(){
 		return myHealthReps;
+	}
+	
+	public int getSpeedReps(){
+		return mySpeedReps;
 	}
 }
