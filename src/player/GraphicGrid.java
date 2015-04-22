9ba2424775e3e15d0af2a49c36680c7ad2f58c05
fangyichen <tomorrowfchen@gmail.com>
2015-04-21 21:53:56 -0400

    add graphicgrid class which implements observer for updating with engin grid

---
+++ src/player/GraphicGrid.java
@@ -0,0 +1,14 @@
+package player;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class GraphicGrid implements Observer{
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
