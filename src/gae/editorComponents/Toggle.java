ff6c1d54c8d7cfc85e9e5d5601106342ec7777e9
REYINA SENATUS <RKS26@DUKE.EDU>
2015-04-23 11:03:26 -0400

    Fixed slider and toggle to include editor component

--- src/gae/editorComponents/Toggle.java
+++ src/gae/editorComponents/Toggle.java
@@ -1,11 +1,15 @@
 package gae.editorComponents;
 
+import gae.model.Receiver;
+
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 import javafx.scene.Node;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
 
 /**
  * This class is to set up a toggle button for the editors This class can be
@@ -16,14 +20,43 @@ import javafx.scene.control.ToggleGroup;
  *
  */
 
-public class Toggle {// TODO: Implement this with EditorComponent
+public class Toggle extends EditorComponent{
+	private String text;
+	private String[] texts;
+	
+	public Toggle(Receiver receiver, Method setMethod, Method getMethod,String objectName) {
+		super(receiver, setMethod, getMethod, objectName);
+		// TODO Auto-generated constructor stub
+	}
 
-	public Node toggleButton(String text) {
+	//@Override
+	public void setUpEditor() {//TODO: Find a way to do this with string
+			// TODO Auto-generated method stub
+			HBox h = new HBox();
+			h.getChildren().add(toggleButton());
+			
+	}
+	
+	public void toggleButtonParam(String myText){
+		text = myText;
+	}
+	
+	public void groupToggleParam(String[] myTexts){
+		texts = new String[myTexts.length];
+		texts = myTexts;
+	}
+		
+	public Node toggleButton() {
 		ToggleButton tgb1 = new ToggleButton(text);
 		return tgb1;
 	}
+	
+	private Node toggleButton(String s) {
+		ToggleButton tgb1 = new ToggleButton(s);
+		return tgb1;
+	}
 
-	public List<Node> toggleGroups(List<String> texts) {
+	public List<Node> toggleGroups() {
 		List<Node> myGroupButtons = new ArrayList<Node>();
 		int i = 0; // iterate through the booleans
 		ToggleGroup myGroup = new ToggleGroup();
@@ -37,4 +70,5 @@ public class Toggle {// TODO: Implement this with EditorComponent
 
 		return myGroupButtons;
 	}
+
 }
