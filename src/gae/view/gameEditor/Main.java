1d6ab987e043850fd6a0ed1e90304fe4b0a82a83
Sunjeev <svd12@duke.edu>
2015-04-24 01:35:22 -0400

    commented out game editor main method, only for testing purposes

--- src/gae/view/gameEditor/Main.java
+++ src/gae/view/gameEditor/Main.java
@@ -1,26 +1,26 @@
-package gae.view.gameEditor;
-
-import gae.model.Model;
-import gae.model.Receiver;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application{
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		Receiver model = new Model();
-		GameEditor e = new GameEditor(model);
-		primaryStage.setScene(new Scene(e.drawGameEditor()));
-		primaryStage.setHeight(600);
-		primaryStage.setWidth(600);
-		primaryStage.show();
-		
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-
-}
+//package gae.view.gameEditor;
+//
+//import gae.model.Model;
+//import gae.model.Receiver;
+//import javafx.application.Application;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
+//
+//public class Main extends Application{
+//	
+//	@Override
+//	public void start(Stage primaryStage) throws Exception {
+//		Receiver model = new Model();
+//		GameEditor e = new GameEditor(model);
+//		primaryStage.setScene(new Scene(e.drawGameEditor()));
+//		primaryStage.setHeight(600);
+//		primaryStage.setWidth(600);
+//		primaryStage.show();
+//		
+//	}
+//	
+//	public static void main(String[] args) {
+//		launch(args);
+//	}
+//
+//}
