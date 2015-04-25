6e8b538eca8a1ad0181f88a730b54f89c4f70b32
REYINA SENATUS <RKS26@DUKE.EDU>
2015-04-25 11:07:15 -0400

    Remaking the old editors

---
+++ src/gae/view/editorpane/EffectEditor.java
@@ -0,0 +1,18 @@
+package gae.view.editorpane;
+
+/**
+ * Editor for Effects of the Bases
+ * @author ReyinaSenatus
+ *
+ */
+
+public class EffectEditor {
+	//TODO: (int) Speed Damage
+	//TODO: (Double) speedFrequency
+	//TODO: (int) speedReps
+	//TODO: (Double) holdSpeedDamageFor (0== Forever)
+	//TODO: (int) Health Damage
+	//TODO: (Double) healthFrequency
+	//TODO: (int) healthReps
+
+}
