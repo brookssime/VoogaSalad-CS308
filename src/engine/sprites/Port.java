0d2550f56bcb6d5ae421918fed1a56e1a253261c
patricktyndall <patrick.wickham@duke.edu>
2015-04-27 00:03:09 -0400

    Permanant objects (Base, Port) have myLocation field for authoring purposes

--- src/engine/sprites/Port.java
+++ src/engine/sprites/Port.java
@@ -1,16 +1,28 @@
 package engine.sprites;
 
+import java.awt.Point;
+
 import engine.gameLogic.Placement;
 
 public class Port extends Sprite{
 
+	Point myLocation;
+	
 	public Port(){
 		
+	}
+	
+	public void setLocation(Point location){
+		myLocation = location;
+	}
+	
+	public Point getLocation(){
+		return myLocation;
 	}
 
 	@Override
 	public Placement move() {
-		return null;
+		return new Placement(myLocation); // REVIEW: Make sure this doesn't ever need to return an orientation...currently set to null
 	}
 
 	@Override
