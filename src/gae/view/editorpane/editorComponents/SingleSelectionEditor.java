f2758331d34e003350107db3e66cdb318ac59d0c
Sunjeev <svd12@duke.edu>
2015-04-29 16:48:44 -0400

    single selection editor complete

--- src/gae/view/editorpane/editorComponents/SingleSelectionEditor.java
+++ src/gae/view/editorpane/editorComponents/SingleSelectionEditor.java
@@ -1,16 +1,24 @@
 package gae.view.editorpane.editorComponents;
 
 import gae.model.Receiver;
-import gae.view.editorpane.ComponentsDialog;
 import interfaces.TypeAnnotation;
 
 import java.lang.reflect.Method;
+import java.util.Set;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class SingleSelectionEditor extends EditorComponent{
+
+	private Button openSelection;
+
 
 	public SingleSelectionEditor(Receiver receiver, Method method,
 			String objectName) {
@@ -21,26 +29,39 @@ public class SingleSelectionEditor extends EditorComponent{
 	@Override
 	public void setUpEditor() {
 		
-		HBox root = new HBox(5);
-		Label selectText = new Label("Select Object");
-		
 		TypeAnnotation typeAnnotation = myMethod
 				.getAnnotation(TypeAnnotation.class);
 		
 		String type = typeAnnotation.type();
-		System.out.println(type);
 		
-		String buttonString = myFetchedValue == null ? "" : myFetchedValue.toString();
-		Button openComponentsDialog = new Button(buttonString);
-		openComponentsDialog.setOnAction(e -> {
-			ComponentsDialog dialog = new ComponentsDialog(type, myReceiver);
-			String ret = dialog.getElement();
-			openComponentsDialog.setText(ret);
-			myReceiver.runOnObjectSwap(myObject, myMethod, ret);
+		String buttonString = myFetchedValue == null ? "Select Object" : myFetchedValue.toString();
+		openSelection = new Button(buttonString);
+		openSelection.setOnAction(e -> {
+			Stage stage = new Stage();
+			stage.setScene(new Scene(createSelection(stage, type)));
+			stage.show();
 		});
 		
-		root.getChildren().addAll(selectText, openComponentsDialog);
-		this.getChildren().add(root);
+		this.getChildren().add(openSelection);
+	}
+
+
+	private Parent createSelection(Stage stage, String type) {
+		VBox root = new VBox(5);
+		ListView<String> selection = new ListView<String>();
+		ObservableList<String> data = FXCollections.observableArrayList();
+		Set<String> objects = (Set<String>) myReceiver.getList(type);
+		data.addAll(objects);
+		selection.setItems(data);
+		
+		Button accept = new Button("Accept");
+		accept.setOnAction(e -> {
+			myReceiver.runOnObjectSwap(myObject, myMethod, selection.getSelectionModel().getSelectedItem());
+			openSelection.setText(selection.getSelectionModel().getSelectedItem());
+			stage.close();
+		});
+		root.getChildren().addAll(selection, accept);
+		return root;
 	}
 
 }
